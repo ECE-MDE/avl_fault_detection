603c7ad47c0ebaf41f5630af8bca0dab3b37766b
Stephen Welch <stephenwelchva@vt.edu>
2023-02-24 19:57:47 +0000

    Add topic dump

--- src/listener.py
+++ src/listener.py
@@ -1,1 +1,12 @@
 import rospy
+
+def main():
+    rospy.init_node('listener', anonymous=True)
+    print("\n\n\n\n\n\n\n")
+    for name, type in rospy.get_published_topics():
+        print(f"{name} {type}")
+    rospy.spin()
+    ...
+
+if __name__ == '__main__':
+    main()
