9d2faefc4f2a074b54254a2bb11852c38171d4aa
Stephen Welch <stephenwelchva@vt.edu>
2023-03-06 01:24:13 -0500

    Publish RPM setpoint

--- src/listener.py
+++ src/listener.py
@@ -1,12 +1,46 @@
 import rospy
+from avl_msgs.msg import VehicleStateMsg, Float64SetpointMsg
+"""
+surge velocity
+heave velocity
+depth rate
+
+roll angle
+pitch angle
+yaw angle
+
+roll rate
+pitch rate
+yaw rate
+"""
 
 def main():
+    # Create a node
     rospy.init_node('listener', anonymous=True)
-    print("\n\n\n\n\n\n\n")
+
+    sim_state_print_rate = rospy.Rate(1)
+    setpt_pub_rate = rospy.Rate(1)
+
+    def print_sim_state(msg: VehicleStateMsg):
+        print(f"{msg}\r")
+        sim_state_print_rate.sleep()
+
+
+    # Create a listener for the sim state topic and print the sim state to the terminal every time it's published
+    rospy.Subscriber("/sim/state", VehicleStateMsg, print_sim_state, queue_size=1)
+
+    # Print a list of all currently published topics
     for name, type in rospy.get_published_topics():
         print(f"{name} {type}")
-    rospy.spin()
-    ...
+
+    # Create a publisher for RPM setpoint topic that publishes the `Float64SetpointMsg` topic
+    pub_rpm_setpt = rospy.Publisher("setpoint/rpm", Float64SetpointMsg)
+
+
+    while not rospy.is_shutdown():
+        pub_rpm_setpt.publish(Float64SetpointMsg(True, 10))
+        setpt_pub_rate.sleep()
+        ...
 
 if __name__ == '__main__':
     main()
