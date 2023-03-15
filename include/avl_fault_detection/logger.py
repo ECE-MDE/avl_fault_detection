76addc6c4f7e87f4fdfefe98b0d9dc0d55578456
Stephen Welch <stephenwelch@vt.edu>
2023-03-15 05:35:11 -0400

    Fix importing other files into Python executables

--- include/avl_fault_detection/logger.py
+++ include/avl_fault_detection/logger.py
@@ -1,18 +1,19 @@
 from __future__ import annotations
+import time
 from typing import List
 import genpy
 import rospy
-import util
+from avl_fault_detection import util
 from pathlib import Path
 
 
 class AvlLogger:
 
-    _AVL_LOG_DIR = Path('/var/avl_logs')
+    _AVL_LOG_DIR = Path('/var/avl_logs/current/log')
 
     def __init__(self, name: str):
         self.name = name
-        self.log_path = Path(AvlLogger._AVL_LOG_DIR, util.get_newest_subdir(AvlLogger._AVL_LOG_DIR), name)
+        self.log_path = Path(AvlLogger._AVL_LOG_DIR, f"{name}.log")
         self.log_file = None
 
     def write_msg_header(self, msg: genpy.Message, units: List[str] | None = None):
@@ -26,6 +27,8 @@ class AvlLogger:
     def write_header(self, names: List[str], units: List[str]):
         if self.log_file:
             rospy.logwarn_once("You probably called write header twice!")
+        names = ["time_s"] + names
+        units = ["none"] + units
         self.log_file = open(self.log_path, 'w')
         self.log_file.write(' '.join(names) + '\n')
         self.log_file.write(' '.join(units) + '\n')
@@ -34,7 +37,8 @@ class AvlLogger:
         if not self.log_file:
             rospy.logerr("You must call write_header before calling write_data")
         else:
-            self.log_file.write(' '.join(str(data)) + '\n')
+            data = [time.time()] + data
+            self.log_file.write(' '.join([str(d) for d in data]) + '\n')
 
     def flush(self):
         if self.log_file:
--- include/avl_fault_detection/util.py
+++ include/avl_fault_detection/util.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 
 def get_newest_subdir(path: Path):
-    all_subdirs = [d for d in os.listdir(path.stem()) if os.path.isdir(d)]
+    all_subdirs = [d for d in os.listdir(path.stem) if os.path.isdir(d)]
     return max(all_subdirs, key=os.path.getmtime)
