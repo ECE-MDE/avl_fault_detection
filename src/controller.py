5281b9434a86e77cdd79e81ff140918915b04f27
Stephen Welch <stephenwelchva@vt.edu>
2023-03-17 01:42:33 -0400

    Updates

--- src/controller.py
+++ src/controller.py
@@ -1,7 +1,7 @@
 import math
 import rospy
 from avl_fault_detection import logger
-from avl_msgs.msg import VehicleStateMsg, Float64SetpointMsg
+from avl_msgs.msg import VehicleStateMsg, Float64SetpointMsg, ActuatorControlMsg
 """
 surge velocity
 heave velocity
@@ -20,35 +20,38 @@ def main():
     # Create a node
     rospy.init_node('controller', anonymous=True)
 
-    sim_state_print_rate = rospy.Rate(1)
-    setpt_pub_rate = rospy.Rate(1)
-
-    # Create a subscriber for the sim state topic and print the sim state to the terminal every 1 second
-    def print_sim_state(msg: VehicleStateMsg):
-        print(f"{msg}\r")
-        sim_state_print_rate.sleep()
-
-    # Create RPM and pitch setpoint publishes to get the AUV moving
-    pub_rpm_setpt = rospy.Publisher("setpoint/rpm", Float64SetpointMsg)
-    pub_pit_setpt = rospy.Publisher("setpoint/pitch", Float64SetpointMsg)
-
-    rospy.Subscriber("/sim/state", VehicleStateMsg, print_sim_state, queue_size=1)
+    start_time = rospy.Time.now()
+    logging_rate_hz = rospy.Rate(10)
+    setpt_pub_rate_hz = rospy.Rate(1)
 
     log = logger.AvlLogger('controller')
-    log.write_header(["fault_type"], ["none"])
+    log.write_msg_header([VehicleStateMsg], ["fault_type"], units=["none"])
+
+    # Create a subscriber for the sim state topic and print the sim state to the terminal every 1 second
+    def log_sim_state(msg: VehicleStateMsg):
+        fault_type = "none" if rospy.Time.now() - start_time < rospy.Duration(15) else "depth_sensor_zero"
+        log.write_msg_data([msg], [fault_type])
+        log.flush() # for debugging purposes
+        print(f"{msg}\r")
+        # print(f"{15} {math.radians(15)} {math.degrees(msg.pitch)} {msg.pitch}\r")
+        logging_rate_hz.sleep()
+
+    # Create RPM and pitch setpoint publishes to get the AUV moving
+    pub_rpm_setpt = rospy.Publisher("setpoint/rpm", Float64SetpointMsg, queue_size=100)
+    pub_pit_setpt = rospy.Publisher("setpoint/pitch", Float64SetpointMsg, queue_size=100)
+    pub_act_ctl = rospy.Publisher("device/actuator_control", ActuatorControlMsg, queue_size=100)
+
+    rospy.Subscriber("/sim/state", VehicleStateMsg, log_sim_state)
 
     # Print a list of all currently published topics
-    for name, topic_type in rospy.get_published_topics():
-        print(f"{name} {topic_type}")
+    # for name, topic_type in rospy.get_published_topics():
+    #     print(f"{name} {topic_type}")
 
     while not rospy.is_shutdown():
+        pub_act_ctl.publish(ActuatorControlMsg(True))
         pub_rpm_setpt.publish(Float64SetpointMsg(True, 10))
-        pub_pit_setpt.publish(Float64SetpointMsg(True, math.radians(-45)))
-        if rospy.Time.now() < 15:
-            log.write_data(["none"])
-        else:
-            log.write_data(["depth_sensor_zeroed"])
-        setpt_pub_rate.sleep()
+        pub_pit_setpt.publish(Float64SetpointMsg(True, math.radians(15)))
+        setpt_pub_rate_hz.sleep()
         ...
     
     log.close()
